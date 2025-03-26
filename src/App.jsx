62423c59cf11f87de1cfe8cc7ebd413fdbecce53
Shriya Singh <2001shriya@gmail.com>
2025-03-26 11:33:44 +0530

    Updated app.jsx for question-1

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function Greeting({name}){
-  return <h2>Hello, {name}</h2>;
+// How do you create a functional component that accepts multiple props (name, age) and displays them?
+// 💡 Modify the Greeting component to take both name and age as props and display them.
+function Greeting({name,age}){
+  return <><h2>Hello, {name}</h2>
+
+  <h2>Age, {age}</h2></>
+  
 }
 
 function App() {
   return <div>
   <h1>functional component</h1>
-  <Greeting name={"sony"}></Greeting>
+  <Greeting name={"GoraPakora"} age={5000}></Greeting>
   </div>
 
 
