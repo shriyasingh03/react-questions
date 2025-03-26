f51a85ea145448ccf60c395eb1002603d650dfe2
Shriya Singh <2001shriya@gmail.com>
2025-03-26 11:17:35 +0530

    added functional component Greeting in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Greeting({name}){
+  return <h2>Hello, {name}</h2>;
+}
+
 function App() {
   return <div>
-    <h1>Hello world</h1>
+  <h1>functional component</h1>
+  <Greeting name={"sony"}></Greeting>
   </div>
+
+
 
 }
 
